247cc7eba698efdbdf0d82833303256f9046595a
Anton Tivonenko <rosgear.it@gmail.com>
2026-06-17 15:47:07 +0300

    Файл конфигурации версии модуля.

---
+++ config/.version.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения RosGear.
+ * 
+ * Файл конфигурации версии модуля.
+ * 
+ * @link https://rosgear.ru/
+ * @copyright Copyright (c) 2015 RosGear
+ * @license https://rosgear.ru/license/
+ */
+
+return [
+    'name'        => 'Visual editor',
+    'description' => 'Website page builder',
+    'version'     => '1.0',
+    'versionDate' => '01-07-2015',
+    'author'      => 'RosGear',
+    'authorUrl'   => 'https://rosgear.ru/',
+    'email'       => 'info@rosgear.ru',
+    'url'         => 'https://rosgear.ru/component/rg-be-site_markup/',
+    'license'     => 'The MIT License',
+    'licenseUrl'  => 'https://rosgear.ru/license/#license-mit'
+];
